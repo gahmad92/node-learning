7954c34b4fb40d04c6e026e641c4f9ef55e00344
gahmad92 <proghulam141@gmail.com>
2025-12-22 15:01:43 +0500

    making of full crud operations in terminal

--- fsCrud.js
+++ fsCrud.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs');
+const { argv } = require('process');
 
 const operation = process.argv[2]
 
@@ -12,4 +13,26 @@ if (operation === "write") {
 
     console.log("file created successfully");
     
+}
+else if (operation ==="read") {
+    const name  = process.r=argv[3]
+    const fullName = "files/"+name+".txt"
+    let data = fs.readFileSync(fullName,"utf8")
+
+    console.log(data);
+    
+}
+else if (operation === "update") {
+    const name = process.argv[3];
+    const content = process.argv[4];
+    const fullName = "files/"+name+".txt"
+
+    let data = fs.appendFileSync(fullName,content)
+    console.log(data);
+    
+} else if (operation === "delete") {
+    const name  = process.argv[3]
+    const fullName = "files/"+name+".txt"
+    fs.unlinkSync(fullName)
+
 }
