a709d8b192ae19c511ec61448cd87e3b900e3ce3
gahmad92 <proghulam141@gmail.com>
2025-12-23 14:12:12 +0500

    playing with it

--- website.js
+++ website.js
@@ -1,38 +1,42 @@
-const http = require("http");
-const fs = require("fs");
+const http = require ('http')
+const fs = require ('fs')
+const path = require ('path')
 
-http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("html/header.html", "utf-8", (err, headerData) => {
-      if (err) {
-        res.writeHead(500, { "content-type": "text/plain" });
-        res.end("internal server error (header)");
-        return;
-      }
-      fs.readFile("html/index.html", "utf-8", (err, indexData) => {
-        if (err) {
-          res.writeHead(500, { "content-type": "text/plain" });
-          res.end("internal server error (index)");
-          return;
-        }
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end(headerData + indexData);
-      });
-    });
-  } else if (req.url === "/style.css") {
-    fs.readFile("html/style.css", "utf-8", (err, cssData) => {
-      if (err) {
-        res.writeHead(404, { "content-type": "text/plain" });
-        res.end("failed to load css");
-        return;
-      }
-      res.writeHead(200, { "content-type": "text/css" });
-      res.end(cssData);
-    });
-  } else {
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.end("404 not found");
-  }
-}).listen(3210, () => {
-  console.log("server running on port 3210");
-});
+
+http.createServer((req,res)=>{
+
+    let filePath
+
+    switch (req.url) {
+        case '/':
+            filePath = "html/index.html"
+            break;
+        case '/about':
+            filePath = "html/about.html"
+            break;
+        case '/contact':
+            filePath = "html/contact.html"
+
+        default:
+            res.writeHead(500,{"content-type":"text/plain"})
+            res.end("internal server error")
+            return
+    }
+    let ext = path.extname(filePath);
+    let contentType = "text/html"
+    if (ext === ".css")  contentType = 'text/css'
+        
+        fs.readFile(filePath,"utf-8", (error, data)=>{
+            if (error) {
+                res.writeHead(500,{"content-type":"text/plain"})
+                res.end("internal server error ")
+                return
+            }
+            res.writeHead(200,{"content-type":contentType})
+            res.end(data)
+        })
+    
+
+}).listen(3210,()=>{
+
+})
